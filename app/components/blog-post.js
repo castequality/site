ce9a0f761c203d4cebe5c2dff99d7c1ea52bed6f
Sean Doyle <sean.p.doyle24@gmail.com>
2014-11-08 18:24:41 -0500

    Match Store Carousel Styles
    * Add social links
    * Add brain logo

--- app/components/blog-post.js
+++ app/components/blog-post.js
@@ -1,6 +1,9 @@
 import Ember from "ember";
 
 export default Ember.Component.extend({
+  classNameBindings: [":post"],
+  tagName: "article",
+
   didInsertElement: function() {
     this.$().find("iframe").attr("width", 775).attr("height", 432);
   }
